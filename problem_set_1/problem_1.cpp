6e1b6ddd31197f241cdcea1b15a0942d8fff70f5
Ivan-Asen Chakarov <vankata9907@gmail.com>
2018-11-05 21:56:11 +0200

    Fixed problem_1

--- problem_set_1/problem_1.cpp
+++ problem_set_1/problem_1.cpp
@@ -2,16 +2,21 @@
 
 using namespace std;
 
-double get_function_result(double x)
-{
-	double result = (11 * x * x + 3 * x + 15) / (x * x * x - 25);
-	return result;
-}
-
 int main()
 {
 	double x;
 	cin >> x;
-	cout << get_function_result(x) << endl;
+
+	double top = 11 * x * x + 3 * x + 15;
+	double bottom = x * x * x - 25;
+
+	if (bottom == 0)
+	{
+		cout << "Can\'t divide by 0 :(" << endl;
+	}
+
+	double result = top / bottom;
+	cout << result << endl;
+	
 	return 0;
 }
