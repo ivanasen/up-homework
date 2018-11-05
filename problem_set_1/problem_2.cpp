b39043e9e704bdd7d12babc4409aaaf809bb35fe
Ivan-Asen Chakarov <vankata9907@gmail.com>
2018-11-05 21:40:15 +0200

    Switched to enum class

--- problem_set_1/problem_2.cpp
+++ problem_set_1/problem_2.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
-enum Position { OUTSIDE, BORDER, INSIDE };
+enum class Position
+{
+	OUTSIDE,
+	BORDER,
+	INSIDE
+};
 
 Position collides_rect(double point_x, double point_y, double x_rect, double y_rect, double w, double h)
 {
 	if ((point_x > x_rect) && (point_x < x_rect + w) && (point_y > y_rect) && (point_y < y_rect + h))
 	{
-		return INSIDE;
+		return Position::INSIDE;
 	}
 
 	bool is_on_top_or_bottom_border = (point_x > x_rect && point_x < x_rect + w) &&
@@ -17,50 +22,50 @@ Position collides_rect(double point_x, double point_y, double x_rect, double y_r
 									  (point_x == x_rect || point_x == x_rect + w);
 	if (is_on_top_or_bottom_border || is_on_left_or_right_border)
 	{
-		return BORDER;
+		return Position::BORDER;
 	}
 
-	return OUTSIDE;
+	return Position::OUTSIDE;
 }
 
 Position evaluate_point(double x, double y)
 {
 	// Check smallest inner rect
-	short collideInnerSmall = collides_rect(x, y, 3, -0.5, 2.5, 2 / 5);
-	if (collideInnerSmall == INSIDE)
+	Position collideInnerSmall = collides_rect(x, y, 3, -0.5, 2.5, 2 / 5);
+	if (collideInnerSmall == Position::INSIDE)
 	{
-		return OUTSIDE;
+		return Position::OUTSIDE;
 	}
-	if (collideInnerSmall == BORDER)
+	if (collideInnerSmall == Position::BORDER)
 	{
 		//If the point is on the left or top border of the smallest rect then it's outside
 		if (x == 3 || y == -0.5 + 2.5)
 		{
-			return OUTSIDE;
+			return Position::OUTSIDE;
 		}
-		return BORDER;
+		return Position::BORDER;
 	}
 
 	// Check medium inner rect
-	short collideInnerMedium = collides_rect(x, y, 3, 2, 4, 2.5);
-	if (collideInnerMedium == INSIDE)
+	Position collideInnerMedium = collides_rect(x, y, 3, 2, 4, 2.5);
+	if (collideInnerMedium == Position::INSIDE)
 	{
-		return OUTSIDE;
+		return Position::OUTSIDE;
 	}
-	if (collideInnerMedium == BORDER)
+	if (collideInnerMedium == Position::BORDER)
 	{
-		return BORDER;
+		return Position::BORDER;
 	}
 
 	// Chech biggest inner rect
-	short collideInnerBig = collides_rect(x, y, -1, -2, 4, 4);
-	if (collideInnerBig == INSIDE)
+	Position collideInnerBig = collides_rect(x, y, -1, -2, 4, 4);
+	if (collideInnerBig == Position::INSIDE)
 	{
-		return OUTSIDE;
+		return Position::OUTSIDE;
 	}
-	if (collideInnerBig == BORDER)
+	if (collideInnerBig == Position::BORDER)
 	{
-		return BORDER;
+		return Position::BORDER;
 	}
 
 	return collides_rect(x, y, -4, -2.5, 13, 8.5); //Is it in the big rect
@@ -72,7 +77,7 @@ int main()
 	cin >> x >> y;
 
 	Position result = evaluate_point(x, y);
-	cout << result;
+	cout << (int)result;
 
 	return 0;
 }
